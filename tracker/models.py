f63ec0d63063d77680ed26cefc0f9345a75b3c43
danil <vinniopo@mail.ru>
2024-07-08 07:34:44 +0700

    created Item and Price models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
+from urllib.parse import urlparse
+
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=512, null=False)
+    url = models.URLField(max_length=1024, null=False)
+
+    def __str__(self):
+        return f'{self.name[:100]} [{urlparse(self.url).hostname}]'
+        
+
+class Price(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
